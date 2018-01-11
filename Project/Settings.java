90374e720886881e0eee310cf812aa18caeb51f3
victorlin7534 <vlin5@stuy.edu>
2018-01-10 19:37:32 -0500

    Implemented ArrayLists of HashMaps and added flow.pdf

--- Project/Settings.java
+++ Project/Settings.java
@@ -8,11 +8,45 @@ public class Settings{
 
   public static boolean atParty(int lineNum){
     File file = new File("Party.txt");
+    return talk(file,lineNum);
+  }
+
+  public static boolean atSchool(int lineNum){
+    File file = new File("School.txt");
+    return talk(file,lineNum);
+  }
+
+  // public static boolean sickness(){
+  //   File file = new File("");
+  //   return talk(file,lineNum);
+  // }
+  //
+  // public static boolean carHit(){
+  //   File file = new File("");
+  //   return talk(file,lineNum);
+  // }
+  //
+  public static boolean takeTest(int line){
+    File file = new File("Test.txt");
+    return talk(file,lineNum);
+  }
+
+  public static boolean atHospital(int line){
+    File file = new File("Hospital.txt");
+    return talk(file,lineNum);
+  }
+
+  public static boolean atWork(int line){
+    File file = new File("Work.txt");
+    return talk(file,lineNum);
+  }
+
+  public static boolean talk(File a, int lineNum){
     try {
       //Create a new Scanner object which will read the data from the file passed in.
       //Check if there is another line and loop through until desired line number is found.
       //When desired line number found, print it.
-      Scanner scanner = new Scanner(file);
+      Scanner scanner = new Scanner(a);
       int lineCtr = 1;
       while (scanner.hasNextLine()) {
         String line = scanner.nextLine();
@@ -27,12 +61,4 @@ public class Settings{
     }
     return true;
   }
-
-//testing
-/*  public static void main(String[] args){
-    atParty(1);
-    atParty(2);
-    atParty(3);
-  }*/
-
 }//end class
--- Project/Woo.java
+++ Project/Woo.java
@@ -1,7 +1,31 @@
 import cs1.Keyboard;
+import java.util.*;
 
 public class Woo{
 
+  private ArrayList stuff;
+
+  public void create(){
+    HashMaps STDS = new HashMaps<String,double>(3);
+    STDS.put("HIV",xcvckvncxvncxv);
+    STDS.put("Syphilis",xcvckvncxvncxv);
+    STDS.put("Chlamidya",xcvckvncxvncxv);
+    stuff.add(STDS);
+    HashMaps MENTAL = new HashMaps<String,double>(3);
+    MENTAL.put("Depression",adsadasb);
+    MENTAL.put("Anxiety",adsadasb);
+    MENTAL.put("Schizophrenia",adsadasb);
+    stuff.add(MENTAL);
+    HashMaps PHYSICAL = new HashMaps<String,double>(3);
+    PHYSICAL.put("Flu",adsadasb);
+    PHYSICAL.put("Cancer",adsadasb);
+    PHYSICAL.put("Heart Disease",adsadasb);
+    stuff.add(PHYSICAL);
+    HashMaps FRIENDS = new HashMaps<String,String>();
+    FRIENDS.put();
+    FRIENDS.put();
+    stuff.add(FRIENDS);
+  }
 
   public static void main(String []args){
 
@@ -10,7 +34,7 @@ public class Woo{
     Human a = new Child(Keyboard.readString());
     //life method found in human
     while(a.life()){
-      a.stats();    
+      a.stats();
 
     }
   }
